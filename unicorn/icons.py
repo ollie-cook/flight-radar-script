74c5c306a9b464f5c7fabdba60f31fa0edfa12f5
Oliver Cook <olliewatford@gmail.com>
2024-10-30 17:41:24 +0000

    updated plane icon

--- unicorn/icons.py
+++ unicorn/icons.py
@@ -8,11 +8,11 @@ def clearLetters():
 
 def plane(x,y,rgb,t=0):
         for j in range(4):
-                uh.set_pixel(x % 17, y+j, rgb[0], rgb[1], rgb[2])
+                uh.set_pixel(x % 17, y+1+j, rgb[0], rgb[1], rgb[2])
 
-        for i in range(2):
-                for j in range(6):
-                        uh.set_pixel((x+(3*i)) % 17, y+j+1, rgb[0], rgb[1], rgb[2])
+        for j in range(3):
+                for i in range(10):
+                        uh.set_pixel((x+1+i) % 17, y+2+j, rgb[0], rgb[1], rgb[2])
         uh.show()
 
         if t > 0:

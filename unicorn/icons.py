8489f3c2028a126b3f881811d3155aaece15db65
Oliver Cook <olliewatford@gmail.com>
2024-11-08 15:47:52 +0000

    Update icons.py

--- unicorn/icons.py
+++ unicorn/icons.py
@@ -7,12 +7,17 @@ def clearLetters():
         uh.clear()
 
 def plane(x,y,rgb,t=0):
-        for j in range(4):
+        for j in range(5):
                 uh.set_pixel(x % 17, y+1+j, rgb[0], rgb[1], rgb[2])
 
         for j in range(3):
                 for i in range(10):
                         uh.set_pixel((x+1+i) % 17, y+2+j, rgb[0], rgb[1], rgb[2])
+
+        for i in range(2):
+                for j in range(2):
+                        uh.set_pixel()
+
         uh.show()
 
         if t > 0:
